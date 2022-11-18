d5f55fb8450a28abf47c17fe42e46dfac81e25ac
Jay V <air@live.ca>
2022-11-18 14:55:17 -0500

    Fixing styles for mobile

--- web/src/config.ts
+++ web/src/config.ts
@@ -20,14 +20,14 @@ import ryanName from "./assets/speakers/ryan-name.svg";
 import ryanNameSmall from "./assets/speakers/ryan-name-small.svg";
 
 import mongo from "./assets/companies/mongo.svg";
-import mongoMono from "./assets/companies/mongo.svg";
+import mongoMono from "./assets/companies/mongo-mono.svg";
 import kevin from "./assets/speakers/kevin.png";
 import kevinSmall from "./assets/speakers/kevin-small.png";
 import kevinName from "./assets/speakers/kevin-name.svg";
 import kevinNameSmall from "./assets/speakers/kevin-name-small.svg";
 
 import sst from "./assets/companies/sst.svg";
-import sstMono from "./assets/companies/sst.svg";
+import sstMono from "./assets/companies/sst-mono.svg";
 import dax from "./assets/speakers/dax.png";
 import daxSmall from "./assets/speakers/dax-small.png";
 import daxName from "./assets/speakers/dax-name.svg";
@@ -59,8 +59,8 @@ interface Speaker {
   title: string;
   company: string;
   color: string;
-  talkTitle: string;
-  talkDescription: string;
+  talkTitle?: string;
+  talkDescription?: string;
   logo: Image;
   logoSmall: Image;
   logoMono: Image;
@@ -87,9 +87,8 @@ export const SPEAKERS: Record<string, Speaker> = {
     title: "CEO at Astro",
     company: "Astro",
     color: "hsla(22, 100%, 50%, 1)",
-    talkTitle: "The Future of SPAs",
-    talkDescription:
-      "In this talk we'll look at how Astro is making SPAs a lot better for building serverless applications.",
+    talkTitle: undefined,
+    talkDescription: undefined,
     logo: { src: astro, width: "243", height: "80" },
     logoSmall: { src: astro, width: "235", height: "68" },
     logoMono: { src: astroMono, width: "144", height: "47" },
@@ -107,14 +106,14 @@ export const SPEAKERS: Record<string, Speaker> = {
     title: "CEO at Upstash",
     company: "Upstash",
     color: "hsla(162, 100%, 46%, 1)",
-    talkTitle: "The Future of Redis",
+    talkTitle: "Serverless Data and Upstash",
     talkDescription:
-      "In this talk we'll look at how Upstash is making Redis a lot better for building serverless applications.",
+      "Why we started Upstash? What use cases do we excel in? How are we different from other Redis/Kafka providers? What makes Upstash true serverless? And how is Upstash designed for the Edge?",
     logo: { src: upstash, width: "297", height: "86" },
     logoSmall: { src: upstash, width: "235", height: "68" },
     logoMono: { src: upstashMono, width: "152", height: "44" },
     logoMonoSmall: { src: upstashMono, width: "110", height: "32" },
-    profile: { src: enes, width: "526", height: "526" },
+    profile: { src: enes, width: "730", height: "580" },
     profileSmall: { src: enesSmall, width: "94", height: "94" },
     nameImage: { src: enesName, width: "534", height: "68" },
     nameImageSmall: { src: enesNameSmall, width: "226", height: "104" },
@@ -127,9 +126,8 @@ export const SPEAKERS: Record<string, Speaker> = {
     title: "Creator of SolidJS",
     company: "SolidJS",
     color: "hsla(211, 43%, 51%, 1)",
-    talkTitle: "The Future of Redis",
-    talkDescription:
-      "In this talk we'll look at how Upstash is making Redis a lot better for building serverless applications.",
+    talkTitle: undefined,
+    talkDescription: undefined,
     logo: { src: solid, width: "382", height: "71" },
     logoSmall: { src: solid, width: "235", height: "68" },
     logoMono: { src: solidMono, width: "164", height: "30" },
@@ -147,9 +145,9 @@ export const SPEAKERS: Record<string, Speaker> = {
     title: "Principal Product Manager at MongoDB",
     company: "MongoDB",
     color: "hsla(79, 100%, 51%, 1)",
-    talkTitle: "The Future of Redis",
+    talkTitle: "MongoDB Atlas",
     talkDescription:
-      "In this talk we'll look at how Upstash is making Redis a lot better for building serverless applications.",
+      "MongoDB Atlas: the most flexible data platform for building serverless applications. Enjoy the flexibility of MongoDB with the added benefit of instant, elastic scaling and an operations-based pricing model that only charges for what you use. Join Kevin Jernigan, Principal Product Manager at MongoDB and Suraj Shetty, Senior Solutions Architect to learn more.",
     logo: { src: mongo, width: "363", height: "91" },
     logoSmall: { src: mongo, width: "235", height: "68" },
     logoMono: { src: mongoMono, width: "156", height: "39" },
@@ -167,9 +165,8 @@ export const SPEAKERS: Record<string, Speaker> = {
     title: "Core Team at SST",
     company: "SST",
     color: "hsla(39, 95%, 77%, 1)",
-    talkTitle: "The Future of Redis",
-    talkDescription:
-      "In this talk we'll look at how Upstash is making Redis a lot better for building serverless applications.",
+    talkTitle: undefined,
+    talkDescription: undefined,
     logo: { src: sst, width: "281", height: "81" },
     logoSmall: { src: sst, width: "235", height: "68" },
     logoMono: { src: sstMono, width: "114", height: "33" },
@@ -187,9 +184,9 @@ export const SPEAKERS: Record<string, Speaker> = {
     title: "Developer Advocate at Neon",
     company: "Neon",
     color: "hsla(64, 95%, 60%, 1)",
-    talkTitle: "The Future of Redis",
+    talkTitle: "Neon — Serverless Postgres",
     talkDescription:
-      "In this talk we'll look at how Upstash is making Redis a lot better for building serverless applications.",
+      "In this presentation, we’ll see how decoupling Postgres’ storage and compute means for developers.",
     logo: { src: neon, width: "293", height: "82" },
     logoSmall: { src: neon, width: "235", height: "68" },
     logoMono: { src: neonMono, width: "126", height: "35" },
