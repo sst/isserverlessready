983e2b7de4d3a614522cc9f0f4b93418fb9dd607
Jay V <air@live.ca>
2022-12-14 17:12:37 -0500

    Updating speaker share and adding prefetch

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   build: {
-    // Example: Generate `page.html` instead of `page/index.html` during build.
+    // Example: Generate `page.html` instead of `page/index.html` during build
     format: "file"
-  }
+  },
+  integrations: [prefetch({
+    // Prefetch all links
+    selector: "a"
+  })]
 });
